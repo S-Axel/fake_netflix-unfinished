67d4901256bdf1c84c3445b41e250ab28cda6d13
S-Axel <sabotieraxel@gmail.com>
2022-05-05 18:47:10 +0200

    LoginPage:  - add background image  - make header, logo and background responsive

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
 import LogoNetflix from '../../components/LogoNetflix';
+import './styles.css';
+import backgroundImg from '../../assets/images/logged_out_background.jpg';
 
 function LoginPage() {
   return (
-    <div>
-      <header>
+    <div className="login-page">
+      <div className="background" aria-hidden="true">
+        <img className="background__img" src={backgroundImg} alt="background" />
+      </div>
+      <header className="header">
         <Link to="/">
-          <LogoNetflix />
+          <LogoNetflix className="header__logo" />
         </Link>
       </header>
     </div>
