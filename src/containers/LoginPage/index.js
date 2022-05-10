d6f9600441d58d3d7c58f515c49bfcecbe014231
S-Axel <sabotieraxel@gmail.com>
2022-05-10 18:35:18 +0200

    LoginPage: Add email or phone field with new TextField component

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 import './styles.css';
 import LogoNetflix from '../../components/LogoNetflix';
 import backgroundImg from '../../assets/images/logged_out_background.jpg';
+import TextField from '../../components/TextField';
 
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    emailOrPhone: '',
+  });
+
+  const onChangeHandler = (e) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div className="login-page">
       <div className="background" aria-hidden="true">
@@ -11,6 +25,20 @@ function LoginPage() {
       <header className="header">
         <LogoNetflix className="header__logo" />
       </header>
+      <main className="login-form__container">
+        <div className="login-form">
+          <h1>Sign In</h1>
+          <form onSubmit={() => null}>
+            <TextField
+              label="Email or phone number"
+              id="emailOrPhone"
+              name="emailOrPhone"
+              value={formData.emailOrPhone}
+              onChange={onChangeHandler}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
