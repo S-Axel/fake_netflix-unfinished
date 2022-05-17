b334204586f6227f81a4d797127c5739622ae834
S-Axel <sabotieraxel@gmail.com>
2022-05-17 10:14:01 +0200

    jsdoc:  - Add jsdoc to the project, also using better-docs for react components  - Modify Button component props  - Add doc to Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,6 +5,11 @@ import PropTypes from 'prop-types';
 
 import './styles.css';
 
+/**
+ * A simple button component
+ *
+ * @component
+ */
 function Button({ className, type, children, ...rest }) {
   const rootClassNames = classNames('rc-button', className);
 
@@ -21,13 +26,20 @@ function Button({ className, type, children, ...rest }) {
 
 Button.propTypes = {
   className: PropTypes.string,
-  type: PropTypes.string,
-  children: PropTypes.node.isRequired,
+  /**
+   * HTML button type attribute, one of [ 'button' | 'submit' | 'reset' ]
+   */
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  /**
+   * Content, text inside the button
+   */
+  children: PropTypes.node,
 };
 
 Button.defaultProps = {
   className: '',
   type: 'button',
+  children: '',
 };
 
 export default memo(Button);
