7ef90c41581094574994b2b5e55ecb3b9ed061af
S-Axel <sabotieraxel@gmail.com>
2022-06-15 12:52:52 +0200

    Fix BrowseHeader tests

--- src/components/BrowsePage/BrowseHeader/index.test.js
+++ src/components/BrowsePage/BrowseHeader/index.test.js
@@ -2,12 +2,19 @@ import { render, screen } from '@testing-library/react';
 import BrowseHeader from './index';
 
 const LOGO_NETFLIX = 'LogoNetflix';
-const LogoNetflix = () => (LOGO_NETFLIX);
-jest.mock('../../LogoNetflix', () => LogoNetflix);
+jest.mock('../../LogoNetflix', () => () => <span>{LOGO_NETFLIX}</span>);
+
+const PRIMARY_NAVIGATION = 'PrimaryNavigation';
+jest.mock('./PrimaryNavigation', () => () => <span>{PRIMARY_NAVIGATION}</span>);
+
+const SECONDARY_NAVIGATION = 'SecondaryNavigation';
+jest.mock('./SecondaryNavigation', () => () => <span>{SECONDARY_NAVIGATION}</span>);
 
 describe('BrowseHeader', () => {
   it('Renders LogoNetflix', () => {
     render(<BrowseHeader />);
     expect(screen.getByText(LOGO_NETFLIX)).toBeInTheDocument();
+    expect(screen.getByText(PRIMARY_NAVIGATION)).toBeInTheDocument();
+    expect(screen.getByText(SECONDARY_NAVIGATION)).toBeInTheDocument();
   });
 });
